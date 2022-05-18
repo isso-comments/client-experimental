5602572f77ca60fd1541ff96a962923c90b44e26
ix5 <ix5@users.noreply.github.com>
2022-05-18 13:01:02 +0200

    STASH import globals

---
+++ src/globals.js
@@ -0,0 +1,18 @@
+var Offset = function() {
+  this.values = [];
+};
+
+Offset.prototype.update = function(remoteTime) {
+  this.values.push((new Date()).getTime() - remoteTime.getTime());
+};
+
+Offset.prototype.localTime = function() {
+  return new Date((new Date()).getTime() - this.values.reduce(
+    function(a, b) { return a + b; }) / this.values.length);
+};
+
+var offset = new Offset();
+
+module.exports = {
+  offset: offset,
+}
