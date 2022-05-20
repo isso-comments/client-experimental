27e99bdf4df634ad29e7c2d7772a55f550e3055d
ix5 <ix5@users.noreply.github.com>
2022-05-20 23:39:20 +0200

    STASH i18n import and fix uk translations

--- src/i18n/uk.js
+++ src/i18n/uk.js
@@ -2,24 +2,24 @@ module.exports = {
   "postbox-text": "Введіть коментар тут (принаймні 3 символи)",
   "postbox-author": "Ім'я (необов'язково)",
   "postbox-author-placeholder": "John Doe",
-  "postbox-email": "Електронна пошта (необов'язково)",
+  "postbox-email": "Пошта (необов'язково)",
   "postbox-email-placeholder": "johndoe@example.com",
   "postbox-website": "Веб-сайт (необов'язково)",
   "postbox-website-placeholder": "https://example.com",
-  "postbox-preview": "Попередній перегляд",
+  "postbox-preview": "Прев'ю",
   "postbox-edit": "Редагувати",
-  "postbox-submit": "відправляти",
-  "postbox-notification": "Підпишіться на повідомлення про відповіді електронною поштою",
+  "postbox-submit": "Відправити",
+  "postbox-notification": "Підписатися на повідомлення про відповіді поштою",
 
   "num-comments": "один коментар\n{{ n }} коментарі\n{{ n }} коментарів",
-  "no-comments": "Коментарів поки немає",
-  "atom-feed": "Атомний потік",
+  "no-comments": "Коментарів поки нема",
+  "atom-feed": "Атомна стрічка",
 
   "comment-reply": "Відповісти",
   "comment-edit": "Редагувати",
   "comment-save": "Зберегти",
   "comment-delete": "Видалити",
-  "comment-confirm": "Підтвердьте",
+  "comment-confirm": "Підтвердити",
   "comment-close": "Закрити",
   "comment-cancel": "Скасувати",
   "comment-deleted": "Коментар видалено.",
@@ -34,5 +34,5 @@ module.exports = {
   "date-day": "Вчора\n{{ n }} дні тому\n{{ n }} днів тому",
   "date-week": "минулого тижня\n{{ n }} тижні тому\n{{ n }} тижнів тому",
   "date-month": "останній місяць\n{{ n }} місяці тому\n{{ n }} місяців тому",
-  "date-year": "торік\n{{ n }} роки тому\n{{ n }} років тому"
+  "date-year": "минулого року\n{{ n }} роки тому\n{{ n }} років тому"
 };
